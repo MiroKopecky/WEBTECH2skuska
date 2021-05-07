0ee1a236358f8be3e346975053c623fa3fb4fa65
DESKTOP-1K5NEAQ\Admin <miroslav.s.kopecky@gmail.com>
2021-05-07 14:51:09 +0200

    draw, link, select

---
+++ src/teacher/create_q_select.php
@@ -0,0 +1,86 @@
+<?php
+require_once "../config.php";
+
+if (isset($_POST['question'])) {
+    $question = $_POST['question'];
+    $answerTrue = $_POST['answerTrue'];
+    $answers = $_POST["answer"];
+    $test_id = 1;
+    $type = "select";
+    $false_answer = [];
+
+    foreach ($answers as $answer) {
+        array_push($false_answer,$answer);
+    }
+
+    try {
+        $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("INSERT INTO testQuestions (question, answer, false_answer, test_id, type) VALUES (:question, :answer, :false_answer, :test_id, :type)");
+        $stmt->bindParam(':question',$question);
+        $stmt->bindParam(':answer',$answerTrue);
+        $stmt->bindParam(':false_answer',$false_answer);
+        $stmt->bindParam(':test_id',$test_id);
+        $stmt->bindParam(':type',$type);
+        $stmt->execute();
+    }
+    catch (PDOException $exception){
+        echo "Error." . $exception;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+<button class="btn add-answer">Pridaj odpoveď</button>
+<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+    <label for="question">Zadaj otázku: </label><br>
+    <input type="text" size="50" name="question" id="question" required>
+    <br>
+    <div class="answers">
+        <div>
+            <label for="answerTrue">Správna odpoveď: </label><br>
+            <input type="text" name="answerTrue" required/>
+        </div><br>
+        <p>Nesprávne odpovede</p>
+        <div class="input-box">
+            <input type="text" name="answer[]" required/>
+        </div>
+    </div>
+    <input type="submit" id="submit" value="Submit" />
+</form>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+
+        let max_input = 8;
+        let counter = 1;
+
+        $('.add-answer').click(function (e) {
+            e.preventDefault();
+            if (counter < max_input) {
+                counter++;
+                $('.answers').append(`
+                    <div>
+                      <input type="text" name="answer[]" required/>
+                      <a href="#" class="remove-answer">Odstráň odpoveď</a>
+                    </div>
+                `);
+            }
+        });
+
+        $('.answers').on("click", ".remove-answer", function (e) {
+            e.preventDefault();
+            $(this).parent('div').remove();
+            counter--;
+        })
+
+    });
+</script>
+</body>
+</html>
