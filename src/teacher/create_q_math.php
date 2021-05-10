e0f86878be7976e57883142afb3bbee583ccb5ca
MartinZofcik <66427822+MartinZofcik@users.noreply.github.com>
2021-05-10 13:32:30 +0200

    Martin vytvorenie otazky

---
+++ src/teacher/create_q_math.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js"></script>
+
+    <title>Matematicka otazka</title>
+</head>
+
+<body>
+
+    Odoslat do DB: <span id="answer">x=</span><br>
+
+    <button id="send">Odoslat</button><br><br>
+
+
+    <script>
+
+
+        var MQ = MathQuill.getInterface(2);
+
+        var answerSpan = document.getElementById('answer');
+
+        var enteredMath = "";
+
+        var firstMathField = MQ.MathField(answerSpan, {
+            handlers: {
+                edit: function() {
+                    enteredMath = firstMathField.latex();
+                    //console.log(enteredMath);
+                    }
+                }
+        });
+
+
+        document.getElementById("send").addEventListener("click", function (){
+           console.log(enteredMath);
+
+           const xhr = new XMLHttpRequest();
+
+           xhr.open("POST", "sendToDB.php");
+           xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+           xhr.send("eq=" + enteredMath);
+
+        });
+
+
+    </script>
+</body>
+
+</html>
