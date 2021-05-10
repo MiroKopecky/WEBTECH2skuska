a70a3a8b4038f21b623157ef1f99418d42f4a712
MartinZofcik <66427822+MartinZofcik@users.noreply.github.com>
2021-05-10 14:55:04 +0200

    uz posiela do DB aj zadanie otazky

--- src/teacher/sendToDB.php
+++ src/teacher/sendToDB.php
@@ -1,12 +1,15 @@
 <?php
 
-$equation = $_POST['eq'];
+$question = $_POST['question'];
+$answer = $_POST['answer'];
+
 
 require_once "Database.php";
 
 $conn = (new Database())->getConnection();
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$stmt = $conn->prepare("INSERT INTO equation (eq) VALUES (:eq)");///pls zmen t podla tvojej DB, ja som to zatial daval len na svoju
-$stmt->bindParam(':eq', $equation);
+$stmt = $conn->prepare("INSERT INTO equation (question,answer) VALUES (:question, :answer)");///pls zmen t podla tvojej DB, ja som to zatial daval len na svoju
+$stmt->bindParam(':question',$question);
+$stmt->bindParam(':answer',$answer);
 $stmt->execute();
